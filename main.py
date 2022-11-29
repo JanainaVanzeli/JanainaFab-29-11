13f9c9d49514cc1904601d7ee1383e541a316e6b
Janaina Vanzeli <janavanzeli@gmail.com>
2022-11-29 14:04:09 -0300

    Continuacao

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ print(compare_values)
 rows, cols = np.where(compare_values == False)
 
 for item in zip(rows, cols):
-    df_prod.iloc[item[0]], item[1]] = '{} =-> {}'. format(df_prod.iloc[item[0], item[1], df_branch.iloc[item[0], item[1]]])
+    df_prod.iloc[item[0], item[1]] = '{} =-> {}'.format(df_prod.iloc[item[0], item[1], df_branch.iloc[item[0], item[1]]])
 
 df_prod.to_excel('files/output.xls', index=False, header=True)
 
